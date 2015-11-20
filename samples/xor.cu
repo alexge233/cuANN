8af466a90f6c79c40a08ae734861bb362030bb3b
Alex Ge <alexge233@hotmail.com>
2015-11-20 05:24:13 +0000

    alpha version

--- samples/xor.cu
+++ samples/xor.cu
@@ -12,15 +12,39 @@ int main (void)
     cuANN::data train_data = cuANN::data( "xor.data" );
 
     // Train on this data, MSE, Epochs
-    float mse = xor_net.train( train_data, 0.05f, 50000, 5000, false );
+    float mse = xor_net.train( train_data, 0.05f, 100000, 5000, false );
     std::cout << "Trained Network with MSE: " << mse << std::endl;
 
     std::cout << "Testing with [1,0] as input" << std::endl;
-    float x_in[2] {1.f, 0.f};
-    thrust::device_vector<float> in_vec(x_in,x_in+2);
-    thrust::device_vector<float> output = xor_net.propagate( in_vec );
+    float x_in1[2] {1.f, 0.f};
+    thrust::device_vector<float> in_vec1(x_in1,x_in1+2);
+    auto output1 = xor_net.propagate( in_vec1 );
     std::cout << "output: ";
-    for ( auto val : output )
+    for ( auto val : output1 )
+        std::cout << val << std::endl;
+
+    std::cout << "Testing with [0,1] as input" << std::endl;
+    float x_in2[2] {0.f, 1.f};
+    thrust::device_vector<float> in_vec2(x_in2,x_in2+2);
+    auto output2 = xor_net.propagate( in_vec2 );
+    std::cout << "output: ";
+    for ( auto val : output2 )
+        std::cout << val << std::endl;
+
+    std::cout << "Testing with [0,0] as input" << std::endl;
+    float x_in3[2] {0.f, 0.f};
+    thrust::device_vector<float> in_vec3(x_in3,x_in3+2);
+    auto output3 = xor_net.propagate( in_vec3 );
+    std::cout << "output: ";
+    for ( auto val : output3 )
+        std::cout << val << std::endl;
+
+    std::cout << "Testing with [1,1] as input" << std::endl;
+    float x_in4[2] {1.f, 1.f};
+    thrust::device_vector<float> in_vec4(x_in4,x_in4+2);
+    auto output4 = xor_net.propagate( in_vec4 );
+    std::cout << "output: ";
+    for ( auto val : output4 )
         std::cout << val << std::endl;
 
     return 0;
